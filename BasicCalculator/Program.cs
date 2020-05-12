679c743b7c36efe52d402cc9eea56fd9cdda346f
zahra1993a <33681568+zahra1993a@users.noreply.github.com>
2020-05-12 12:36:36 +0200

    Basic calculator done

--- BasicCalculator/Program.cs
+++ BasicCalculator/Program.cs
@@ -6,7 +6,38 @@ namespace BasicCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Cal(5,"+",7) ;
+
+        }
+
+        static void Cal(int num1, string op, int num2)
+        {
+            int result = 0;
+
+            switch (op)
+            {
+                case "-":
+                    result = num1 - num2;
+                    break;
+
+                case "+":
+                    result = num1 + num2;
+                    break;
+
+                case "*":
+                    result = num1 * num2;
+                    break;
+
+                case "/":
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Enter valid operatior");
+                    break;
+            }
+            Console.WriteLine($"{num1} {op} {num2} = {result}");
+
         }
     }
 }
+
